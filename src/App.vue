52c5560e049eb3b78e9efb3a4a465a67c7dae270
Pablo Sirera <psirera4@gmail.com>
2020-05-26 18:18:43 +0200

    feat: add new font

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,11 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Muli:wght@400;500;700&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Muli', Avenir, Helvetica, Arial, sans-serif;
+  font-weight: normal;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
