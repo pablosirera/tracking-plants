0d3a6cf3f71b8adaaba96cb901d2b207977f0bfa
Pablo Sirera <psirera4@gmail.com>
2020-06-23 19:20:12 +0200

    feat(auth): add auth required on vue router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import { ROUTES } from '@/constants'
 import Home from '../views/home/index.vue'
+import firebase from 'firebase'
 
 Vue.use(VueRouter)
 
@@ -9,17 +10,26 @@ const routes = [
   {
     path: ROUTES.HOME.path,
     name: ROUTES.HOME.name,
+    meta: {
+      authRequired: true
+    },
     component: Home
   },
   {
     path: ROUTES.ADD.path,
     name: ROUTES.ADD.name,
+    meta: {
+      authRequired: true
+    },
     component: () => import('@/views/add/index.vue')
   },
   {
     path: ROUTES.DETAIL.path,
     name: ROUTES.DETAIL.name,
     props: true,
+    meta: {
+      authRequired: true
+    },
     component: () => import('@/views/detail/index.vue')
   },
   {
@@ -33,6 +43,15 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser
+  const requiresAuth = to.matched.some(record => record.meta.authRequired)
+
+  if (requiresAuth && !currentUser) next({ path: ROUTES.AUTH.path })
+  else if (!requiresAuth && currentUser) next()
+  else next()
 })
 
 export default router
